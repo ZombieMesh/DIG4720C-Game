8c32f4e6bf850977652e1ec668fa2daa64a6f0db
JammyJamz <33839743+JammyJamz@users.noreply.github.com>
2019-09-30 10:42:40 -0400

    first update

---
+++ Assets/Scripts/AddButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour
+{
+    [SerializeField]
+    private Transform puzzleField;
+    
+    public int amountOfButtons = 20;
+
+    [SerializeField]
+    private GameObject btn;
+
+    private void Awake()
+    {
+        for (int i = 0; i < amountOfButtons; i++)
+        {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(puzzleField, false);
+        }
+
+    }
+   
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public List<Button> btns = new List<Button>();
+    public Sprite image;
+
+    private void Start()
+    {
+        GetButtons();
+    }
+
+    void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
+        for (int i = 0; i < objects.Length; i++)
+        {
+            btns.Add(objects[i].GetComponent<Button>());
+            btns[i].image.sprite = image;
+        }
+    }
+   
+}
