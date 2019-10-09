61977d745a3dff2994795a968fc80d3b275e736a
Ivana-Pagan <37190657+Ivana-Pagan@users.noreply.github.com>
2019-10-09 00:28:36 -0400

    Lose and Win UI Scripting
    Added the needed scripts for UI functionion on Lose and Win Screen

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Load Quit"); //Confirming this is quiting while in Editor.
+        Application.Quit();
+    }
+}
