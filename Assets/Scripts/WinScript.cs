c7abb303d62091446b17b039749b664dd04f612e
Yuriy <42846093+ukiyo-3@users.noreply.github.com>
2019-10-09 10:01:30 -0400

    Fixed WinCondition

--- Assets/Scripts/WinScript.cs
+++ Assets/Scripts/WinScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class WinScript : MonoBehaviour
+public class WinCondition : MonoBehaviour
 {
     public void LoadMenu()
     {
