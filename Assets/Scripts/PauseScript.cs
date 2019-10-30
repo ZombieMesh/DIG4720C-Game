dd23e92da23a0befb38fd76e20c9d08e8f8d9353
Ivana-Pagan <37190657+Ivana-Pagan@users.noreply.github.com>
2019-10-30 03:36:47 -0400

    Worked on Stuff
    Created pause button, added prize scenes

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    bool isPaused = false;
+   
+
+    public void pauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1f;
+            isPaused = false;   
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
